add logging to trainer, correct formula for vtarg

snake-ai/snake_ai/model.py
@@ -16,6 +16,7 @@ class ActorCritic(nn.Module):
         num_actions: int,
         emb_dim: int,
         kernel_size: int,
+        hidden_dim: int,
         dropout_rate: float = 0.1,
     ):
         # convolutional network
@@ -29,20 +30,30 @@ class ActorCritic(nn.Module):
         self.dropout = nn.Dropout(dropout_rate)
         self.preconv = nn.Linear(input_shape[-1], emb_dim)
         c = emb_dim
-        self.conv1 = nn.Conv2d(
-            c, c // 2, kernel_size=kernel_size, stride=2, padding=0, dilation=1
+        self.convs = []
+        while h * w * c > hidden_dim * 2:
+            conv = nn.Conv2d(
+                c, c // 2, kernel_size=kernel_size, stride=2, padding=0, dilation=1
+            )
+            h = int((h - (kernel_size - 1) - 1) / 2 + 1)
+            w = int((w - (kernel_size - 1) - 1) / 2 + 1)
+            c //= 2
+            self.convs.append(conv)
+        self.convs = nn.ModuleList(self.convs)
+        self.prob_dense = nn.Sequential(
+            nn.Linear(h * w * c, hidden_dim),
+            self.gelu,
+            nn.Linear(hidden_dim, hidden_dim),
+            self.gelu,
+            nn.Linear(hidden_dim, num_actions),
         )
-        h = int((h - (kernel_size - 1) - 1) / 2 + 1)
-        w = int((w - (kernel_size - 1) - 1) / 2 + 1)
-        c //= 2
-        self.conv2 = nn.Conv2d(
-            c, c // 4, kernel_size=kernel_size, stride=2, padding=0, dilation=1
+        self.val_dense = nn.Sequential(
+            nn.Linear(h * w * c, hidden_dim),
+            self.gelu,
+            nn.Linear(hidden_dim, hidden_dim),
+            self.gelu,
+            nn.Linear(hidden_dim, 1),
         )
-        h = int((h - (kernel_size - 1) - 1) / 2 + 1)
-        w = int((w - (kernel_size - 1) - 1) / 2 + 1)
-        c //= 4
-        self.prob_dense = nn.Linear(h * w * c, num_actions)
-        self.val_dense = nn.Linear(h * w * c, 1)
 
     def forward(self, states: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """
@@ -58,13 +69,15 @@ class ActorCritic(nn.Module):
                 of the shape (B,)
         """
 
-        x: torch.Tensor = self.gelu(self.preconv(states.float()))
+        x: torch.Tensor = self.gelu(
+            self.preconv(states.float().unsqueeze(1).unsqueeze(1))
+        )
         # B, H, W, C -> B, C, H, W
         x = x.permute(0, 3, 1, 2).contiguous()
 
         # pass through shared convs
-        x = self.dropout(self.selu(self.conv1(x)))
-        x = self.dropout(self.selu(self.conv2(x)))
+        for conv in self.convs:
+            x = self.dropout(self.selu(conv(x)))
         x = x.flatten(start_dim=1)
 
         # calculate probability logits and value

snake-ai/snake_ai/trainer.py
@@ -10,11 +10,12 @@ from torch.optim import Optimizer
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from gymnasium import Env
-from typing import List
+from typing import List, Tuple
 from .model import ActorCritic
 from dataclasses import dataclass, field
 from bisect import bisect_right
 from tqdm import tqdm
+from torch.utils.tensorboard.writer import SummaryWriter
 
 
 @dataclass
@@ -25,25 +26,33 @@ class EpisodeRecord:
     actions: torch.Tensor
     probs: torch.Tensor
     advantages: torch.Tensor = field(init=False)
+    target_values: torch.Tensor = field(init=False)
     T: int = field(init=False)
 
     def __post_init__(self):
         self.T = self.states.shape[0]
         self.advantages = None
+        self.target_values = None
 
-    def calculate_advantages(self, gamma: float, lam: float):
+    def calculate(self, gamma: float, lam: float):
         # deltas[i] = how much better state i+1 is compared to state i
         deltas = []
         for t in range(self.T - 1):
             deltas.append(self.rewards[t] + gamma * self.values[t + 1] - self.values[t])
+        deltas.append(0)
+
         # instead of doing the N^2 version of calculating advantages,
         # go from the back instead
         advantages = torch.zeros((len(deltas),))
-        # set the last item, -2 since 0-indexed
-        advantages[self.T - 2] = deltas[self.T - 2]
-        for t in range(self.T - 3, -1, -1):
+        # set the last item
+        advantages[-1] = deltas[-1]
+        for t in range(self.T - 2, -1, -1):
             advantages[t] = deltas[t] + (lam * gamma) * advantages[t + 1]
         self.advantages = advantages
+
+        # also calculate target values
+        # target value should just be the discounted future reward
+        self.target_values = self.advantages + self.values
 
 
 class EpisodeDataset(Dataset):
@@ -67,6 +76,7 @@ class EpisodeDataset(Dataset):
         return {
             "states": item.states[episode_idx],
             "values": item.values[episode_idx],
+            "target_values": item.target_values[episode_idx],
             "actions": item.actions[episode_idx],
             "probs": item.probs[episode_idx],
             "rewards": item.rewards[episode_idx],
@@ -78,20 +88,23 @@ class EpisodeDataset(Dataset):
 
 
 def collect_samples(
-    samples: int, model: ActorCritic, env: Env, device: str
-) -> List[EpisodeRecord]:
+    samples: int, t: int, model: ActorCritic, env: Env, device: str
+) -> Tuple[List[EpisodeRecord], float, float]:
     """
     Collects `samples` samples from the environment, resetting it at first
     Assumes that model.get_value and model.get_action have gradients
 
     Arguments:
         samples (int): the number of samples to collect
+        t (int): the maximum number of samples to collect before resetting the env
         model (ActorCritic): the actor-critic
         env (Env): the environment to use
         device (str): the device to use
 
     Returns:
-        EpisodeRecord: the states, values, and rewards
+        ret (Tuple[List[EpisodeRecord], float, float): a tuple containing
+            the list of episode records, the mean reward, and the mean
+            episode length
     """
     cur_states = []
     cur_values = []
@@ -103,6 +116,9 @@ def collect_samples(
     obs, _ = env.reset()
     num_samples = 0
     cur_length = 0
+    cur_reward = 0
+    total_reward = 0
+    total_length = 0
     while num_samples < samples:
         # take current and prepare to step
         cur_states.append(obs)
@@ -116,8 +132,9 @@ def collect_samples(
         # step in the environment
         obs, reward, terminated, truncated, _ = env.step(action)
         cur_rewards.append(reward)
+        cur_reward += reward
 
-        if terminated or truncated:
+        if terminated or truncated or cur_length >= t:
             if cur_length > 2:
                 # new episode
                 records.append(
@@ -129,10 +146,14 @@ def collect_samples(
                         probs=torch.tensor(cur_probs),
                     )
                 )
+                total_reward += cur_reward
+                total_length += cur_length
+
             cur_rewards = []
             cur_states = []
             cur_values = []
             cur_length = 0
+            cur_reward = 0
             # reset the environment
             obs, _ = env.reset()
         num_samples += 1
@@ -150,7 +171,9 @@ def collect_samples(
                 probs=torch.tensor(cur_probs),
             )
         )
-    return records
+        total_reward += cur_reward
+        total_length += cur_length
+    return records, total_reward / len(records), total_length / len(records)
 
 
 def train(
@@ -158,6 +181,7 @@ def train(
     optimizer: Optimizer,
     env: Env,
     iterations: int,
+    t: int,
     samples: int,
     batch_size: int,
     epochs: int,
@@ -168,6 +192,9 @@ def train(
     ecf: float = 1.0,
     device: str = "cpu",
     num_workers: int = 4,
+    clip_norm: bool = True,
+    clip_norm_val: float = 2.0,
+    normalize_advantages: bool = False,
 ):
     # see page 5 of https://arxiv.org/pdf/1707.06347
     """
@@ -178,6 +205,7 @@ def train(
         optimizer (Optimizer): the optimizer to use
         env (Env): the environment to train in
         iterations (int): the number of iterations to train
+        t (int): the number of samples to take from each environment
         samples (int): the number of samples to collect per iteration
         batch_size (int): the batch size for training
         epochs (int): the number of epochs to train per iteration
@@ -189,14 +217,27 @@ def train(
         device (str, optional): the device to use (cpu or cuda). Defaults to "cpu".
     """
     model.to(device)
+    i = 0
+    writer = SummaryWriter()
+    writer.add_scalar("params/gamma", gamma, 0)
+    writer.add_scalar("params/lambda", lam, 0)
+    writer.add_scalar("params/vcf", vcf, 0)
+    writer.add_scalar("params/ecf", ecf, 0)
+    writer.add_scalar("params/batch_size", batch_size, 0)
+    writer.add_scalar("params/epochs", epochs, 0)
+
     for iteration in range(iterations):
         print(f"iteration {iteration+1}")
         model.eval()
-        episodes = collect_samples(samples, model, env, device)
+        episodes, mean_reward, mean_length = collect_samples(
+            samples, t, model, env, device
+        )
+        writer.add_scalar("env/mean_reward", mean_reward, i)
+        writer.add_scalar("env/mean_length", mean_length, i)
 
         # calculate advantages
         for episode in episodes:
-            episode.calculate_advantages(gamma, lam)
+            episode.calculate(gamma, lam)
 
         # fit actor and critic
         model.train()
@@ -208,19 +249,27 @@ def train(
         total_loss = 0
         total_lclip = 0
         total_lvf = 0
+        total_lentropy = 0
+        total_items = 0
         for _ in range(epochs):
             for batch in loader:
                 # clear old gradients
                 optimizer.zero_grad()
 
                 # get batch data
-                states, actions, probs, rewards, advantages = (
+                states, actions, probs, target_values, advantages = (
                     batch["states"].to(device),
                     batch["actions"].to(device),
                     batch["probs"].to(device),
-                    batch["rewards"].to(device),
+                    batch["target_values"].to(device),
                     batch["advantages"].to(device),
                 )
+
+                # normalize advantages
+                if normalize_advantages and advantages.shape[0] > 1:
+                    advantages = (advantages - advantages.mean()) / (
+                        advantages.std() + 1e-8
+                    )
 
                 # predict
                 pred_probs, pred_vals = model(states)
@@ -229,14 +278,14 @@ def train(
                 cur_action_probs = pred_probs[
                     torch.arange(pred_probs.shape[0]), actions
                 ]
-                ratio = cur_action_probs / probs.where(probs == 0, 1e-8)
+                ratio = cur_action_probs / torch.where(probs == 0, 1e-8, probs)
                 # paper says we want to maximize this, so therefore just negate it
                 lclip = -torch.minimum(
                     ratio * advantages, torch.clip(ratio, 1 - eps, 1 + eps) * advantages
                 ).mean()
 
                 # calculate lvf
-                lvf = torch.mean((pred_vals - rewards) ** 2)
+                lvf = torch.mean((pred_vals - target_values) ** 2)
 
                 # calculate entropy bonus
                 # encourage low probabilities for the current actions
@@ -246,6 +295,8 @@ def train(
                 # backpropagate
                 loss = lclip + vcf * lvf + ecf * lentropy
                 loss.backward()
+                if clip_norm:
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), clip_norm_val)
                 optimizer.step()
 
                 # logging
@@ -253,7 +304,6 @@ def train(
                 lclip = lclip.detach().item()
                 lvf = lvf.detach().item()
                 lentropy = lentropy.detach().item()
-
                 prog.set_postfix(
                     {
                         "loss": loss,
@@ -267,12 +317,37 @@ def train(
                 total_loss += loss
                 total_lclip += lclip
                 total_lvf += lvf
+                total_lentropy += lentropy
                 if np.isnan(loss):
                     print(pred_probs)
                     print(pred_vals)
+                    raise Exception("NAN Values!")
+                writer.add_scalar("train/loss", loss, i)
+                writer.add_scalar("train/lclip", lclip, i)
+                writer.add_scalar("train/lvf", lvf, i)
+                writer.add_scalar("train/lentropy", lentropy, i)
+                writer.add_scalar("train/mean_ratio", ratio.mean().detach().item(), i)
+                writer.add_scalar(
+                    "train/mean_original_probs", probs.mean().detach().item(), i
+                )
+                writer.add_scalar(
+                    "train/mean_new_probs", cur_action_probs.mean().detach().item(), i
+                )
+                writer.add_scalar(
+                    "train/advantages", advantages.mean().detach().item(), i
+                )
+                writer.add_scalar(
+                    "train/target_values", target_values.mean().detach().item(), i
+                )
+                i += 1
+                total_items += 1
+        # more logging
         prog.close()
         print(
-            f"Total loss {total_loss:.4f}, lclip {total_lclip:.4f}, lvf {total_lvf:.4f}"
+            f"Average epoch loss {total_loss/total_items:.4f},"
+            f" lclip {total_lclip/total_items:.4f},"
+            f" lvf {total_lvf/total_items:.4f},"
+            f" lent {total_lentropy/total_items:.4f}"
         )
 
     model.eval()

snake-env/snake_env/snake.py
@@ -35,6 +35,10 @@ class SnakeEnv(gym.Env):
     observation_space = gym.spaces.Box(
         low=0, high=3, shape=(SIZE_X + 2, SIZE_Y + 2, 4), dtype=np.uint8
     )
+    REWARD_SCALE = 0.1
+    EAT_REWARD = 50
+    DEATH_PENALTY = 100
+    INVALID_PENALTY = 10
 
     # display related constants
     FRAME_SCALE = 10
@@ -123,14 +127,27 @@ class SnakeEnv(gym.Env):
         # Making sure the snake cannot move in the opposite direction instantaneously
         change_to = self.ACTION_MAP[action]
 
-        if change_to == "UP" and self.direction != "DOWN":
-            self.direction = "UP"
-        if change_to == "DOWN" and self.direction != "UP":
-            self.direction = "DOWN"
-        if change_to == "LEFT" and self.direction != "RIGHT":
-            self.direction = "LEFT"
-        if change_to == "RIGHT" and self.direction != "LEFT":
-            self.direction = "RIGHT"
+        reward = 0
+        if change_to == "UP":
+            if self.direction != "DOWN":
+                self.direction = "UP"
+            else:
+                reward -= self.INVALID_PENALTY
+        if change_to == "DOWN":
+            if self.direction != "UP":
+                self.direction = "DOWN"
+            else:
+                reward -= self.INVALID_PENALTY
+        if change_to == "LEFT":
+            if self.direction != "RIGHT":
+                self.direction = "LEFT"
+            else:
+                reward -= self.INVALID_PENALTY
+        if change_to == "RIGHT":
+            if self.direction != "LEFT":
+                self.direction = "RIGHT"
+            else:
+                reward -= self.INVALID_PENALTY
 
         # Moving the snake
         if self.direction == "UP":
@@ -147,13 +164,13 @@ class SnakeEnv(gym.Env):
         dist = abs(self.snake_pos[0] - self.food_pos[0]) + abs(
             self.snake_pos[1] - self.food_pos[1]
         )
-        reward = 1 / (dist + 1)
+        reward += self.REWARD_SCALE * ((self.SIZE_X + self.SIZE_Y) / 3 - dist)
         if (
             self.snake_pos[0] == self.food_pos[0]
             and self.snake_pos[1] == self.food_pos[1]
         ):
             self.score += 1
-            reward += 1
+            reward += self.EAT_REWARD
             self._spawn_food()
         else:
             self.snake_body.pop()
@@ -169,7 +186,7 @@ class SnakeEnv(gym.Env):
         # hitting itself
         terminated = terminated or self.snake_pos in list(self.snake_body)[1:]
         if terminated:
-            reward = -1
+            reward = -self.DEATH_PENALTY
 
         # return the observation, reward, terminated, truncated, and info
         observation = self._get_obs()